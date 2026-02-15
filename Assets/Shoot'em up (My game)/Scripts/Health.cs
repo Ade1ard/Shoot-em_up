23009e21f89cfb25b39948999335d8e3669da727
Ade1ard <gorbunovjarroslav@gmail.com>
2026-02-15 13:18:52 +0300

    Enemy health bar

--- Assets/Shoot'em up (My game)/Scripts/Health.cs
+++ Assets/Shoot'em up (My game)/Scripts/Health.cs
@@ -1,15 +1,23 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Health : MonoBehaviour, IDamageable
 {
+    [Header("UI")]
+    [SerializeField] private Image _healthBarAmount;
+    [SerializeField] private float _healthBarDrawingSpeed = 1;
+
     [Header("Parameters")]
     [SerializeField] private float _maxHealth;
 
     private float _currentHealth;
+    private Coroutine _healthBarCoroutine;
 
     protected virtual void Start()
     {
         _currentHealth = _maxHealth;
+        StartDrawingBar();
     }
 
     public virtual void Initialize(float multiplier = default)
@@ -25,6 +33,8 @@ public abstract class Health : MonoBehaviour, IDamageable
         _currentHealth -= damage;
         _currentHealth = Mathf.Clamp(_currentHealth, 0f, _maxHealth);
 
+        StartDrawingBar();
+
         if (_currentHealth <= 0f)
             Death();
     }
@@ -32,6 +42,22 @@ public abstract class Health : MonoBehaviour, IDamageable
     protected virtual void Death()
     {
 
+    }
+
+    private void StartDrawingBar()
+    {
+        if (_healthBarCoroutine == null)
+            _healthBarCoroutine = StartCoroutine(DrawHealthBar());
+    }
+
+    private IEnumerator DrawHealthBar()
+    {
+        while (_healthBarAmount.fillAmount != (_currentHealth / _maxHealth))
+        {
+            _healthBarAmount.fillAmount = Mathf.MoveTowards(_healthBarAmount.fillAmount, _currentHealth / _maxHealth, _healthBarDrawingSpeed / 100);
+            yield return null;
+        }
+        _healthBarCoroutine = null;
     }
 }
 
